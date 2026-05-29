b12cfb94679cd71344b6b4004e7ceb59654ff512
egachan3 <backon4649@gmail.com>
2026-05-29 18:28:24 +0900

    feat: 大カテゴリ選択画面の実装 (#25)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get "large_categories/index"
   devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "large_categories#index"
+
+  resources :large_categories, only: [ :index ] do
+    resources :levels, only: [ :index ], module: :large_categories
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -10,7 +17,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  root "large_categories#index"
 end

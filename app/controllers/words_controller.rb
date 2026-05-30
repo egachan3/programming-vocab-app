15627f2a7e97477a911011c372877073dbfae303
egachan3 <backon4649@gmail.com>
2026-05-30 16:59:48 +0900

    feat: 単語一覧画面の実装 (#29)

---
+++ app/controllers/words_controller.rb
@@ -0,0 +1,17 @@
+class WordsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @large_category = LargeCategory.find(params[:large_category_id])
+    @level = params[:level].to_i
+    @categories = @large_category.categories
+
+    @current_category = if params[:category_id].present?
+                          @categories.find(params[:category_id])
+    else
+                          @categories.first
+    end
+
+    @words = @current_category.words.where(level: @level)
+  end
+end

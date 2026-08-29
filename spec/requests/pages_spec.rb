4aae2e0915190562044b912cdcef1a77b7cc116e
egachan3 <backon4649@gmail.com>
2026-08-29 14:45:35 +0900

    feat: OGP・Twitter Cardのメタタグを追加する
    URLをSlack・X・LINE等に共有した際に、画像・タイトル・説明文つきの
    カードとして表示されるようにする。
    
    - OGP画像(1200x630)は、README用に撮影済みのトップページスクリーン
      ショットからヒーロー部分をPillowで切り出して作成する
    - og:title/og:descriptionはアプリ全体で共通の値を使う
    - canonicalリンクをog:urlと同じ値で追加する
    
    あわせてテストを追加する。
    - og:image/og:title/og:description/twitter:cardが出力されること
    - OGP画像が実際にPNG・1200x630で配信されること

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -32,4 +32,63 @@ RSpec.describe "Pages", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "OGP・Twitter Cardのメタタグ" do
+    it "og:imageに絶対URLでOGP画像を指定している" do
+      get root_path
+
+      page = Capybara::Node::Simple.new(response.body)
+      expect(page).to have_css('meta[property="og:image"][content="http://www.example.com/ogp.png"]', visible: false)
+    end
+
+    it "og:title・og:descriptionが空でない" do
+      get root_path
+
+      page = Capybara::Node::Simple.new(response.body)
+      og_title = page.find('meta[property="og:title"]', visible: false)[:content]
+      og_description = page.find('meta[property="og:description"]', visible: false)[:content]
+
+      expect(og_title).to be_present
+      expect(og_description).to be_present
+    end
+
+    it "twitter:cardがsummary_large_imageになっている" do
+      get root_path
+
+      page = Capybara::Node::Simple.new(response.body)
+      expect(page).to have_css('meta[name="twitter:card"][content="summary_large_image"]', visible: false)
+    end
+
+    # og:urlにトラッキングパラメータ等が付いたままだと、SNS側で
+    # クエリの組み合わせごとに別ページとしてキャッシュされてしまう
+    it "og:urlにクエリパラメータを含めない" do
+      get root_path, params: { utm_source: "x", foo: "bar" }
+
+      page = Capybara::Node::Simple.new(response.body)
+      expect(page).to have_css('meta[property="og:url"][content="http://www.example.com/"]', visible: false)
+    end
+
+    it "canonicalリンクがog:urlと同じ値になっている" do
+      get root_path
+
+      page = Capybara::Node::Simple.new(response.body)
+      og_url = page.find('meta[property="og:url"]', visible: false)[:content]
+      canonical = page.find('link[rel="canonical"]', visible: false)[:href]
+
+      expect(canonical).to eq(og_url)
+    end
+  end
+
+  describe "OGP画像の配信" do
+    it "/ogp.pngがPNGとして配信され、1200x630サイズである" do
+      get "/ogp.png"
+
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq("image/png")
+
+      # PNGヘッダ直後(バイト16〜23)に幅と高さがビッグエンディアンの32bit整数で格納されている
+      width, height = response.body[16, 8].unpack("N2")
+      expect([ width, height ]).to eq([ 1200, 630 ])
+    end
+  end
 end
